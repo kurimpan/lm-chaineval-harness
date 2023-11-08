a6e5f2dd2efae53edf27a3d223b8b1fe45c4c276
Waka Ito <waka.i.0201.mail@gmail.com>
2023-11-08 13:20:18 +0900

    evaluators

--- designed_itowaka/evaluators2.py
+++ designed_itowaka/evaluators2.py
@@ -16,12 +16,12 @@ class Evaluator:
             test_cases = [d[keymap.get('result')] for d in data]
             candidates = [[d['model_output']] for d in data]
             # Compute pass@k for each candidate
-            pass_at_k, results = self.metric.compute(references=test_cases, predictions=candidates, k=[1])
+            score = self.metric.compute(references=test_cases, predictions=candidates, k=[1])['pass@1']
             #pass_at_k, results = code_eval.compute(references=test_cases, predictions=candidates, k=[1])
             #print(pass_at_k)
             #{'pass@1': 1.0}
             # For this example, we are considering pass@1
-            score = pass_at_k
+
         elif self.metric.name == "accuracy":
             # 正解データと予測データのリストを準備
             references = [d[keymap.get('result')] for d in data]
